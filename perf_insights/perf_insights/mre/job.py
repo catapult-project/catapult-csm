2f82e13e8b492801b97bceb91c58d6d9c179716e
Ethan Kuefner <eakuefner@chromium.org>
2015-12-01 13:20:17 -0800

    Added Job

---
+++ perf_insights/perf_insights/mre/job.py
@@ -0,0 +1,17 @@
+# Copyright 2015 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+class Job(object):
+
+  def __init__(self, map_function, reduce_function):
+    self._map_function = map_function
+    self._reduce_function = reduce_function
+
+  @property
+  def map_function(self):
+      return self._map_function
+
+  @property
+  def reduce_function(self):
+      return self._reduce_function
