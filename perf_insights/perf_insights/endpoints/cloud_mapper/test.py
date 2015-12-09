6faaf2fa87d9a1c96cf97d2d286bd7e9b7a60e33
Ethan Kuefner <eakuefner@chromium.org>
2015-12-09 09:19:29 -0800

    perf_insights/function_handle -> perf_insights/mre/function_handle

--- perf_insights/perf_insights/endpoints/cloud_mapper/test.py
+++ perf_insights/perf_insights/endpoints/cloud_mapper/test.py
@@ -24,7 +24,7 @@ Copyright (c) 2015 The Chromium Authors. All rights reserved.
 Use of this source code is governed by a BSD-style license that can be
 found in the LICENSE file.
 -->
-<link rel="import" href="/perf_insights/function_handle.html">
+<link rel="import" href="/perf_insights/mre/function_handle.html">
 <link rel="import" href="/perf_insights/value/value.html">
 <link rel="import" href="/tracing/extras/rail/rail_score.html">
 
